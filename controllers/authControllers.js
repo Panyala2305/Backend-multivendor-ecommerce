c8de0e91ea14fc1d0feb8fd852ca9e5a5c12fa84
Panyala2305 <akashreddy2305@gmail.com>
2025-03-17 10:17:13 -0700

    udated admin and seller models

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,9 +1,12 @@
 const adminModel = require('../models/adminModel')
+const sellerModel = require('../models/sellerModel')
+const sellerCustomerModel  = require('../models/chat/sellerCustomerModel')
 const { responseReturn } = require('../utiles/response')
 const bcrpty = require('bcrypt')
 const { createToken } = require('../utiles/tokenCreate')
 
 class authControllers{
+   
     admin_login = async(req,res) => {
         const {email,password} = req.body
         try {
@@ -38,6 +41,78 @@ class authControllers{
  
     }
     // End Method 
+
+
+    seller_login = async(req,res) => {
+        const {email,password} = req.body
+        try {
+            const seller = await sellerModel.findOne({email}).select('+password')
+            // console.log(admin)
+            if (seller) {
+                const match = await bcrpty.compare(password, seller.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : seller.id,
+                        role : seller.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000 )
+                    }) 
+                    responseReturn(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReturn(res,404,{error: "Password Wrong"})
+                }
+ 
+                 
+            } else {
+                responseReturn(res,404,{error: "Email not Found"})
+            }
+            
+        } catch (error) {
+            responseReturn(res,500,{error: error.message})
+        }
+ 
+    }
+    // End Method 
+
+
+    seller_register = async(req, res) => {
+         const {email,name,password} = req.body
+         try {
+            const getUser = await sellerModel.findOne({email})
+            if (getUser) {
+                responseReturn(res,404,{error: 'Email Already Exit'})
+            }else{
+                const seller = await sellerModel.create({
+                    name,
+                    email,
+                    password: await bcrpty.hash(password, 10),
+                    method : 'menualy',
+                    shopInfo: {}
+                })
+               await sellerCustomerModel.create({
+                     myId: seller.id
+               })
+
+               const token = await createToken({ id : seller.id, role: seller.role })
+               res.cookie('accessToken',token, {
+                expires : new Date(Date.now() + 7*24*60*60*1000 )
+               })
+
+               responseReturn(res,201,{token,message: 'Register Success'})
+            }
+         } catch (error) {
+            responseReturn(res,500,{error: 'Internal Server Error'})
+         }
+    }
+    // End Method 
+
+
+
+
+
+
 
     getUser = async (req, res) => {
         const {id, role} = req;
